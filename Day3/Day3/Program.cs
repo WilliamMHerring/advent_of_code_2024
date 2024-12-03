2e23aa981d8ee871b02304bfff48a793116d9669
Will Herring <William.m.herring@outlook.com>
2024-12-02 21:25:48 -0800

    day 3

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+string input = await File.ReadAllTextAsync("input.txt");
+
+//PART 1
+//string regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)";
+
+//var matches = Regex.Matches(input, regex);
+
+//int total = 0;
+
+//foreach (Match match in matches)
+//{
+//    string text = match.Value.Remove(match.Value.Length - 1, 1).Remove(0, 4);
+//    string[] parts = text.Split(',');
+//    int left = int.Parse(parts[0]);
+//    int right = int.Parse(parts[1]);
+//    total += left * right;
+//}
+
+//Console.WriteLine(total);
+//Console.ReadLine();
+
+//PART 2
+string regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don\\'t\\(\\)";
+
+var matches = Regex.Matches(input, regex);
+
+int total = 0;
+bool multiply = true;
+foreach (Match match in matches)
+{
+    if (match.Value == "do()")
+    {
+        multiply = true;
+        continue;
+    }
+    else if (match.Value == "don't()")
+    {
+        multiply = false;
+        continue;
+    }
+    else if (multiply)
+    {
+        string text = match.Value.Remove(match.Value.Length - 1, 1).Remove(0, 4);
+        string[] parts = text.Split(',');
+        int left = int.Parse(parts[0]);
+        int right = int.Parse(parts[1]);
+        total += left * right;
+    }
+
+}
+
+Console.WriteLine(total);
+Console.ReadLine();
